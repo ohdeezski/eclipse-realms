e93a2e0549137f4aec50df343b44da707469a853
Roberto C. Agosto <roberto2c2agosto2@gmail.com>
2026-08-03 22:50:42 -0400

    chore: update WebSocket connection test

--- client/tests/test_ws_connection.gd
+++ client/tests/test_ws_connection.gd
@@ -3,15 +3,19 @@ func _ready():
     var peer = WebSocketPeer.new()
     peer.connect_to_url("ws://localhost:9051/")
     # Poll a few times to establish connection
-    for i in range(20):
+    for i in range(50):
         peer.poll()
         var state = peer.get_ready_state()
         if state == 0:  # OPEN
-            print("Connected! State:", state)
             # Try to receive data
-            if peer.get_available_packet_count() > 0:
+            while peer.get_available_packet_count() > 0:
                 var packet = peer.get_packet()
                 var text = packet.get_string_from_utf8()
                 print("Received:", text)
+        elif state == 1:  # CLOSING
+            print("Closing...")
+            break
+        elif state == 2:  # CLOSED
+            print("Closed")
             break
     get_tree().quit()
